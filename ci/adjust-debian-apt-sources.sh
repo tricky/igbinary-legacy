616c2dee6ff413c855462f314d4dad62fb54f5e3
thecaliskan <bilgi@emrecaliskan.com.tr>
2025-10-07 01:03:30 +0300

    removed unnecessary if for php 7.0

--- ci/adjust-debian-apt-sources.sh
+++ ci/adjust-debian-apt-sources.sh
@@ -5,8 +5,8 @@
 
 . /etc/os-release
 
-if [ "x${ID:-linux}" = "xdebian" ] && { [ "x${VERSION_CODENAME:-unknown}" = "xbuster" ] || [ "x${VERSION_CODENAME:-unknown}" = "xstretch" ]; } ; then
-    echo "Adjusting APT sources for Debian Buster or Stretch"
+if [ "x${ID:-linux}" = "xdebian" ] && { [ "x${VERSION_ID:-0}" = "x9" ] || [ "x${VERSION_ID:-0}" = "x10" ]; } ; then
+    echo "Adjusting APT sources for Debian Stretch or Buster"
     sed -i 's|http://deb.debian.org/debian|http://archive.debian.org/debian|g' /etc/apt/sources.list
     sed -i 's|http://security.debian.org/debian-security|http://archive.debian.org/debian-security|g' /etc/apt/sources.list
     echo 'Acquire::Check-Valid-Until "false";' > /etc/apt/apt.conf.d/99no-check-valid-until
