be5edc6af73b71461d4ccabfedf74b825126dd67
thecaliskan <bilgi@emrecaliskan.com.tr>
2025-10-07 01:34:28 +0300

    removed unnecessary if for php 7.0

--- ci/adjust-debian-apt-sources.sh
+++ ci/adjust-debian-apt-sources.sh
@@ -5,14 +5,11 @@
 
 . /etc/os-release
 
-if [ "x${ID:-linux}" = "xdebian" ] && { [ "x${VERSION_ID:-0}" = "x9" ] || [ "x${VERSION_ID:-0}" = "x10" ]; } ; then
-    echo "Adjusting APT sources for Debian Stretch or Buster"
+if [ "x${ID:-linux}" = "xdebian" ] && { [ "x${VERSION_CODENAME:-unknown}" = "xbuster" ] || [ "x${VERSION_CODENAME:-unknown}" = "xstretch" ]; } ; then    echo "Adjusting APT sources for Debian Stretch or Buster"
     sed -i 's|http://deb.debian.org/debian|http://archive.debian.org/debian|g' /etc/apt/sources.list
     sed -i 's|http://security.debian.org/debian-security|http://archive.debian.org/debian-security|g' /etc/apt/sources.list
-    cat >> /etc/apt/apt.conf.d/99no-check-valid-until <<'EOF'
-    Acquire::Check-Valid-Until "false";
-    Acquire::AllowInsecureRepositories "true";
-EOF
+    echo 'Acquire::Check-Valid-Until "false";' >> /etc/apt/apt.conf.d/99no-check-valid-until
+    echo 'Acquire::AllowInsecureRepositories "true";' >> /etc/apt/apt.conf.d/99no-check-valid-until
 	if [ "x${VERSION_ID:-0}" = "x9" ]; then
 		sed -i 's|^deb .*stretch-updates|# &|g' /etc/apt/sources.list
 		sed -i 's|^deb .*stretch/updates|# &|g' /etc/apt/sources.list
