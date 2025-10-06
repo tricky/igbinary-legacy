d2462fe7588711ee6638b0ec4b555bed6171d6ca
thecaliskan <bilgi@emrecaliskan.com.tr>
2025-10-07 01:06:51 +0300

    removed unnecessary if for php 7.0

--- ci/adjust-debian-apt-sources.sh
+++ ci/adjust-debian-apt-sources.sh
@@ -10,4 +10,5 @@ if [ "x${ID:-linux}" = "xdebian" ] && { [ "x${VERSION_ID:-0}" = "x9" ] || [ "x${
     sed -i 's|http://deb.debian.org/debian|http://archive.debian.org/debian|g' /etc/apt/sources.list
     sed -i 's|http://security.debian.org/debian-security|http://archive.debian.org/debian-security|g' /etc/apt/sources.list
     echo 'Acquire::Check-Valid-Until "false";' > /etc/apt/apt.conf.d/99no-check-valid-until
+    echo 'Acquire::AllowInsecureRepositories "true";' > /etc/apt/apt.conf.d/99no-check-valid-until
 fi
