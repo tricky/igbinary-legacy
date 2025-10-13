6e212527a46eb515b6eb8d6eb6e263160dfc998d
Emre Çalışkan <13554944+thecaliskan@users.noreply.github.com>
2025-10-13 12:52:51 +0300

    Update adjust-debian-apt-sources.sh

--- ci/adjust-debian-apt-sources.sh
+++ ci/adjust-debian-apt-sources.sh
@@ -5,15 +5,19 @@
 
 . /etc/os-release
 
-if [ "x${ID:-linux}" = "xdebian" ] && { [ "x${VERSION_ID:-0}" = "x9" ] || [ "x${VERSION_ID:-0}" = "x10" ]; } ; then
-    echo "Adjusting APT sources for Debian Stretch or Buster"
+if [ "x${ID:-linux}" = "xdebian" ] && [ "x${VERSION_ID:-0}" = "x10" ]; then
+    echo "Adjusting APT sources for Debian Buster"
+    sed -i 's|http://deb.debian.org/debian|http://archive.debian.org/debian|g' /etc/apt/sources.list
+    sed -i 's|http://security.debian.org/debian-security|http://archive.debian.org/debian-security|g' /etc/apt/sources.list
+fi
+
+if [ "x${ID:-linux}" = "xdebian" ] && [ "x${VERSION_ID:-0}" = "x9" ]; then
+    echo "Adjusting APT sources for Debian Stretch"
     sed -i 's|http://deb.debian.org/debian|http://archive.debian.org/debian|g' /etc/apt/sources.list
     sed -i 's|http://security.debian.org/debian-security|http://archive.debian.org/debian-security|g' /etc/apt/sources.list
     echo 'Acquire::Check-Valid-Until "false";' >> /etc/apt/apt.conf.d/99no-check-valid-until
     echo 'Acquire::AllowInsecureRepositories "true";' >> /etc/apt/apt.conf.d/99no-check-valid-until
-	if [ "x${VERSION_ID:-0}" = "x9" ]; then
-		sed -i 's|^deb .*stretch-updates|# &|g' /etc/apt/sources.list
-		sed -i 's|^deb .*stretch/updates|# &|g' /etc/apt/sources.list
-		echo "Disabled stretch-updates and stretch-security in sources.list to prevent 404 errors"
-	fi
+	echo "Disabled stretch-updates and stretch-security in sources.list to prevent 404 errors"
+	sed -i 's|^deb .*stretch-updates|# &|g' /etc/apt/sources.list
+	sed -i 's|^deb .*stretch/updates|# &|g' /etc/apt/sources.list
 fi
